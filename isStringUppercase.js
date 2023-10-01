4c246dae05b7031a0386d37220c0ba2a59d10275
Akeem Bello <belloakeem07@gmail.com>
2023-10-01 13:28:20 -0600

    Is the string uppercase

---
+++ isStringUppercase.js
@@ -0,0 +1,17 @@
+// Is the string uppercase?
+// Task
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+// Answer
+
+String.prototype.isUpperCase=function() {
+    return this == this.toUpperCase()
+  }
